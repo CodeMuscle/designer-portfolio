4982ca020300cbdba79af0dad577103687a27631
CodeMuscle <investportal99@gmail.com>
2024-09-27 10:12:42 +0530

    Testimonial Section

--- src/sections/Projects.tsx
+++ src/sections/Projects.tsx
@@ -6,6 +6,7 @@ import Image from "next/image";
 
 import CheckCircleIcon from "@/assets/icons/check-circle.svg";
 import ArrowUpRightIcon from "@/assets/icons/arrow-up-right.svg";
+import Card from "@/components/Card";
 
 const portfolioProjects = [
   {
@@ -64,16 +65,10 @@ export const ProjectsSection = () => {
 
         <div className="flex flex-col mt-10 md:mt-20 gap-20 md:gap-24 lg:gap-20">
           {portfolioProjects.map((project) => (
-            <div
+            <Card
               key={project.title}
-              className="bg-gray-800 rounded-3xl relative z-0 overflow-hidden after:z-10 after:content-[''] after:absolute after:inset-0 after:outline-2 after:outline  after:-outline-offset-2 after:rounded-3xl after:outline-white/20 px-8 pt-8 md:pt-12 md:px-10 lg:pt-16 lg:px-20 after:pointer-events-none"
+              className="px-8 pt-8 md:pt-12 pb-0 md:px-10 lg:pt-16 lg:px-20"
             >
-              {/* Grain */}
-              <div
-                className="absolute inset-0 -z-10 opacity-5"
-                style={{ backgroundImage: `url(${grainImage})` }}
-              ></div>
-
               <div className="lg:grid lg:grid-cols-2 lg:gap-16">
                 <div className="lg:pb-16">
                   <div className="bg-gradient-to-r from-emerald-300 to-sky-400 inline-flex font-bold uppercase tracking-widest text-sm gap-2 bg-clip-text text-transparent">
@@ -113,7 +108,7 @@ export const ProjectsSection = () => {
                   />
                 </div>
               </div>
-            </div>
+            </Card>
           ))}
         </div>
       </div>
