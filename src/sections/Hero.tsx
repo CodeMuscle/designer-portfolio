47785b689245153232a60c139d925f3de4d418ea
CodeMuscle <investportal99@gmail.com>
2024-10-04 06:50:31 +0530

    Fixed Z-Index Position Bugs

--- src/sections/Hero.tsx
+++ src/sections/Hero.tsx
@@ -10,7 +10,7 @@ import SparkleIcon from "@/assets/icons/sparkle.svg";
 export const HeroSection = () => {
   return (
     <div className="py-32 md:py-48 lg:py-60 relative z-0 overflow-x-clip">
-      <div className="absolute inset-0 [mask-image:linear-gradient(to_bottom,transparent,black_10%,black_70%,transparent)]">
+      <div className="absolute inset-0 [mask-image:linear-gradient(to_bottom,transparent,black_10%,black_70%,transparent)] -z-10">
         <div
           className="absolute inset-0 -z-30 opacity-5"
           style={{ backgroundImage: `url(${grainImage.src})` }}
@@ -129,13 +129,17 @@ export const HeroSection = () => {
         </div>
 
         <div className="flex flex-col md:flex-row justify-center items-center mt-8 gap-4">
-          <button className="inline-flex items-center gap-2 border border-white/15 px-6 h-12 rounded-xl">
+          <a href="#projects"
+            className="inline-flex items-center gap-2 border border-white/15 px-6 h-12 rounded-xl"
+          >
             <span className="font-semibold">Explore My Work</span>
             <ArrowDown className="size-4" />
-          </button>
+          </a>
           <button className="inline-flex items-center gap-2 border border-white bg-white text-gray-900 px-6 h-12 rounded-xl">
             <span>👋🏼</span>
-            <span className="font-semibold">Let's Connect</span>
+            <span className="font-semibold">
+              <a href="mailto:ganeshgovind11@gmail.com">Let's Connect</a>
+            </span>
           </button>
         </div>
       </div>
